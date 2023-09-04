fbc2be5ff20cf2b1ad5ca8f25dff4a6330ec12d1
constel786 <134733862+constel786@users.noreply.github.com>
2023-09-04 11:09:04 +0300

    code

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         monochrome = true,//makes reports more readable in the console
         features = "./src/test/resources/features",//path of feature file
         glue = "stepdefinitions" //path of the step definitions
-        tags = "@xyz"
+        tags =  //"@xyz"
         dryRun = false //executes to generate missing step definitions
 )
 public class Runner {
