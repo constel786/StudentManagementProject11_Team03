43b752a78b363a7cc0e8c8712fd1ed7161d3b29b
Senem Aydın <dytsenemcelik@gmail.com>
2023-09-05 22:16:46 +0300

    seno03

--- src/test/java/pages/ManagementonSchoolHomePage.java
+++ src/test/java/pages/ManagementonSchoolHomePage.java
@@ -16,10 +16,10 @@ public class ManagementonSchoolHomePage {
     public WebElement login;
 
     @FindBy(xpath = "//input[@id='username']")
-    public WebElement userName;
+    public WebElement userNameLogin;
 
     @FindBy(xpath = "//input[@id='password']")
-    public WebElement password;
+    public WebElement passwordLogin;
 
     @FindBy(xpath = "//button[@class='fw-semibold btn btn-primary']")
     public WebElement loginButton;
@@ -33,4 +33,31 @@ public class ManagementonSchoolHomePage {
 
     @FindBy(linkText = "//input[@id='name']")
     public WebElement surname;
+
+    @FindBy(linkText = "//input[@id='birthPlace']")
+    public WebElement birthPlace;
+
+    @FindBy(linkText = "//input[@id='phoneNumber']")
+    public WebElement phoneNumber;
+
+    @FindBy(linkText = "//input[@value='FEMALE']")
+    public WebElement femaleRadioButton;
+
+    @FindBy(linkText = "//input[@value='MALE']")
+    public WebElement maleRadioButton;
+
+    @FindBy(linkText = "//input[@id='birthDay']")
+    public WebElement dateOfBirth;
+
+    @FindBy(linkText = "//input[@id='ssn']")
+    public WebElement ssn;
+
+    @FindBy(linkText = "//input[@id='username']")
+    public WebElement userNameRegister;
+
+    @FindBy(linkText = "//input[@id='password']")
+    public WebElement passwordRegister;
+    @FindBy(linkText = "//button[.='Register']")
+    public WebElement registerButton;
+
 }
