ecf20d1b20f9737f258847aa40c1a975fa231d8c
humaebrar <humaebrardemr@gmail.com>
2023-09-10 20:00:46 +0300

    EbrarV1

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -7,19 +7,18 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {//report plugins
-                "pretty",//generates reports on the console
+        plugin = {
+                "pretty",
                 "html:target/default-cucumber-reports.html",
                 "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml",
                 "rerun:target/failedRerun.txt",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         },
-        monochrome = true,//makes reports more readable in the console
-        features = "./src/test/resources/features",//path of feature file
-        glue = "stepdefinitions", //path of the step definitions
-        tags = "@us17tc07",
-        dryRun = false //executes to generate missing step definitions
+        features = "./src/test/resources/features",
+        glue = "stepdefinitions",
+        tags = "@us09tc02",
+        dryRun = false
 )
 public class Runner {
 }
