0851a9cd57bc28fe630160fa9e31261e483c06c0
soundsfamiliar <bedreddinalacaogullari@gmail.com>
2023-09-14 16:02:34 +0300

    bedo v.1

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -15,10 +15,10 @@ import org.junit.runner.RunWith;
                 "rerun:target/failedRerun.txt",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         },
-        monochrome = true,//makes reports more readable in the console
+        monochrome = false,//makes reports more readable in the console
         features = "./src/test/resources/features",//path of feature file
         glue = "stepdefinitions", //path of the step definitions
-        tags = "@us17tc07",
+        tags = "@bedo",
         dryRun = false //executes to generate missing step definitions
 )
 public class Runner {
